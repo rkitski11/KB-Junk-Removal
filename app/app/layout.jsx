945eb7af834b6b267a3348213f139ea57e84d796
rkitski11 <rkitski11@gmail.com>
2026-04-01 17:00:39 -0500

    Create layout.jsx

---
+++ app/app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "KB Junk Removal",
+  description: "Junk removal services in Washington County",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
