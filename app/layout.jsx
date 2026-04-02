088935fa9ae3663aeefe7e3812db821cffc17e43
rkitski11 <rkitski11@gmail.com>
2026-04-01 22:02:34 -0500

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,18 @@
+import { Analytics } from '@vercel/analytics/next';
+ 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
 export const metadata = {
   title: "KB Junk Removal",
   description: "Junk removal services in Washington County",
